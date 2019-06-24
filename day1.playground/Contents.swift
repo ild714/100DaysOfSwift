f570a0a6f72a27976ac4a227557a9e1ae6b0c38f
ildar777 <42585624+ildar777@users.noreply.github.com>
2019-06-24 23:11:30 +0300

    Day1

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbuy"
+
+var num = 8_000_000
+
+// Multi-line strings
+var test = """
+This is
+test
+12
+"""
+
+// Double
+var pi = 3.14
+var p = 5
+print (pi + Double(p) )
+
+// Bool
+var quastion = true
+
+// String interpolation
+var score = 85
+var str1 = "Your score was \(score)"
+print ("result:\(str1)")
+
+//Constants
+let taylor = "swift"
+
+//Type
+
+var sum : Int = 53
+var Name : String = "Smit"
+var piDouble : Double = 3.14*2
+let yes : Bool = true
